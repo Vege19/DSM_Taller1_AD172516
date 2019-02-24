55d21b26b6b8b6281cefaf99f4f894c1ae94eed9
Vege19 <josue.ardon113@gmail.com>
2019-02-24 16:49:58 -0600

    Recyclerview with it views is set

---
+++ Taller1_RecyclerView&MapView/app/src/main/java/com/example/taller1_recyclerviewmapview/Adapters/PlaceAdapter.java
@@ -0,0 +1,61 @@
+package com.example.taller1_recyclerviewmapview.Adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.taller1_recyclerviewmapview.Models.Place;
+import com.example.taller1_recyclerviewmapview.R;
+
+import java.util.List;
+
+public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.ViewHolder> {
+
+    private List<Place> places;
+    private Context context;
+
+    public PlaceAdapter(List<Place> places, Context context) {
+        this.places = places;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_place, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+        Place place = places.get(i);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return places.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mPlaceName;
+        private ImageView mPlaceImage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mPlaceImage = itemView.findViewById(R.id.placeImage);
+            mPlaceName = itemView.findViewById(R.id.placeName);
+
+        }
+
+    }
+
+}
